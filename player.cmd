ae876e0d417a1c6c7340e648e156475826b1d690
dfkt <github@dfkt.tk>
2016-03-02 14:02:01 +0100

    Start MPC-HC fullscreen

--- player.cmd
+++ player.cmd
@@ -1,4 +1,6 @@
-@"C:\Program Files\Combined Community Codec Pack 64bit\MPC\mpc-hc64.exe" %*
+@"C:\Program Files\Combined Community Codec Pack 64bit\MPC\mpc-hc64.exe" /fullscreen %*
+
+:: MPC-HC commandline switches: https://gist.github.com/vBm/88abddafd53706439c29
 
 :: Environment variables aren't working with Livestreamer, for some reason:
 :: @"%programfiles%\Combined Community Codec Pack 64bit\MPC\mpc-hc64.exe" %*
